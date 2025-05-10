d81a64f7443dc231de8c03690d5dd172d1d3719c
Satish Singh <i.sksingh113@gmail.com>
2025-05-10 16:33:29 +0530

    authApi

--- src/components/organisms/home/HeroWrapper.tsx
+++ src/components/organisms/home/HeroWrapper.tsx
@@ -1,10 +1,25 @@
+"use client";
 import Section from "@/components/atoms/Section";
 import ContainerBox from "@/components/layout/ContainerBox";
 import MaxWidthWrapper from "@/components/layout/MaxWidthWrapper";
 import HeroCarousel from "@/components/molecules/home/HeroCarousel";
-import React from "react";
+import React ,{useEffect}from "react";
+import { useRouter } from "next/navigation";
+
+
 
 const HeroWrapper = () => {
+  const router = useRouter();
+    useEffect(() => {
+       console.log("Current URL:", window.location.href);
+       const queryParams = new URLSearchParams(window.location.search);
+       const token = queryParams.get("token");
+   
+       if (token) {
+         localStorage.setItem("token", token);
+        router.replace("/"); 
+       }
+     }, [router]);
   return (
     <Section>
       <MaxWidthWrapper>
--- src/store/api/authApi.ts
+++ src/store/api/authApi.ts
@@ -5,25 +5,28 @@ export const authApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     signup: builder.mutation({
       query: (data: FormValues) => ({
-        url: "api/signup",
+        url: "/auth/signup",
         method: "POST",
         body: data,
       }),
     }),
     login: builder.mutation({
       query: (data: { email: string; password: string }) => ({
-        url: "/api/login",
+        url: "/auth/signin",
         method: "POST",
         body: data,
       }),
     }),
-    verifyOtp: builder.mutation({
-      query: (otp: string) => ({
-        url: "api/verify-otp",
-        method: "POST",
-        body: { otp },
-      }),
-    }),
+   verifyOtp: builder.mutation<
+  { message: string; user?: any },
+  { verificationCode: string }
+>({
+  query: (data) => ({
+    url: "/auth/verify-email",
+    method: "POST",
+    body: data,
+  }),
+}),
   }),
   overrideExisting: false,
 });
