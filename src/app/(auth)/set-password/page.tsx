25963b481c4b0bfd7d3eb9d957fb88bd9344e51d
Omar Pervez <freelancertusar5182@gmail.com>
2025-05-22 15:09:03 +0600

    update set passowrd page

--- src/app/(auth)/set-password/page.tsx
+++
@@ -1,5 +0,0 @@
-import SetPasswordWrapper from "@/components/organisms/auth/SetPasswordWrapper";
-
-export default function SetPassword() {
-  return <SetPasswordWrapper />;
-}
--- src/components/organisms/auth/SetPasswordWrapper.tsx
+++ src/components/organisms/auth/SetPasswordWrapper.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import SetPasswordForm from "@/components/molecules/auth/SetPasswordForm";
 import FormHeader from "@/components/molecules/auth/FormHeader";
 import FormText from "@/components/molecules/auth/FormText";
 
-const SetPasswordWrapper = () => {
+type SetPasswordWrapperProps = {
+  token: string;
+};
+
+const SetPasswordWrapper = ({ token }: SetPasswordWrapperProps) => {
+  console.log(token);
+  const router = useRouter();
+
+  useEffect(() => {
+    const expirationTime = 10 * 60 * 1000;
+    const timeout = setTimeout(() => {
+      router.push("/password-reset");
+    }, expirationTime);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <div className="font-montserrat w-full max-w-md mx-auto p-4 md:p-8 bg-white rounded-lg shadow-md">
       <FormHeader>Reset password</FormHeader>
