236a388da5d7a032adf166c9ec437e85c64423dd
demirkrts100 <demirkrts12@gmail.com>
2025-08-12 23:30:27 +0300

    Update FooterWrapper.tsx

--- src/components/organisms/footer/FooterWrapper.tsx
+++ src/components/organisms/footer/FooterWrapper.tsx
@@ -211,7 +211,7 @@ const FooterWrapper = () => {
                 openSection === section.id ? "open" : ""
               }`}
             >
-              <div className="px-[17px] py-2 bg-[#0d0d0d]">{section.content}</div>
+              <div className="px-2 py-2 bg-[#0d0d0d]" overflow-x-hidden"> {section.content}</div>
             </div>
           </div>
         ))}
